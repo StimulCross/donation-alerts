641cc6add81bcb0201f62fcd903fb729ae9f3ea7
StimulCross <stimulcross@gmail.com>
2023-02-27 23:57:03 +0600

    chore: rename formatting script: prettier -> format

--- publish.mjs
+++ publish.mjs
@@ -41,7 +41,7 @@ if (localRev !== remoteRev && remoteRev !== baseRev) {
 
 await runYarn(['rebuild']);
 await runYarn(['lint']);
-await runYarn(['prettier:check']);
+await runYarn(['format:check']);
 
 const versionType = process.argv[2] ?? 'patch';
 
