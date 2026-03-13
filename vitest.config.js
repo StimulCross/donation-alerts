7e4e65d7ebee60dec8b2d6e5c01e45d8c60ff135
Stimul Cross <stimulcross@gmail.com>
2026-03-13 15:32:26 +0500

    test: update config to use rootDir

--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vitest/config';
+import { fileURLToPath } from 'node:url';
+
+const rootDir = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
 	test: {
+		root: rootDir,
 		environment: 'node',
 		globals: false,
 		projects: ['packages/*'],
